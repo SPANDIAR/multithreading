a5f80febbfcedc35c994173c1fcca7c98d5108fe
sankar_pandiarajan <spandiarajan@apple.com>
2020-05-13 19:50:10 -0700

    added exception handler

--- multithreading-basics-01/src/io/spandiar/MultithreadingBasicsOne.java
+++ multithreading-basics-01/src/io/spandiar/MultithreadingBasicsOne.java
@@ -8,18 +8,23 @@ public class MultithreadingBasicsOne {
 			@Override
 			public void run() {
 				System.out.println("Inside my parallel thread " + Thread.currentThread().getName());
+				System.out.println("My priority is " + Thread.currentThread().getPriority());
+				throw new RuntimeException("I'm going rouge");
 			}
 		});
 
 		myThread.setName("myThread");
+		myThread.setPriority(Thread.MAX_PRIORITY);
 		myThread.start();
+		
+		myThread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+			@Override
+			public void uncaughtException(Thread t, Throwable e) {
+				System.out.println("Thread seems to misbehave " + t.getName() + " exception is " + e.getMessage());
+			}
+		});
 
 		System.out.println("Before going to sleep " + Thread.currentThread().getName());
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
 		System.out.println("after waking up " + Thread.currentThread().getName());
 
 	}
